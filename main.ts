1a262b8683f7e1b380f3f87e20ac0deedf4874b9
Antonio Jesús <44335139+antoniojesus156@users.noreply.github.com>
2022-01-31 12:21:02 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+let counter = 0
+input.onLogoEvent(TouchButtonEvent.Released, function () {
+    counter = randint(1, 3)
+    if (counter == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (counter == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (counter == 3) {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
